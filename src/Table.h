50a242c4d92493bbcfcd3383788465d1b0f05ead
Andrew Brinker <me@andrewbrinker.com>
2013-10-30 19:25:44 -0700

    Updated stuff

---
+++ src/Table.h
@@ -0,0 +1,51 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 Andrew Brinker
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+#ifndef SRC_TABLE_H_
+#define SRC_TABLE_H_
+
+#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&);               \
+  void operator=(const TypeName&)
+
+#include <string>
+#include <vector>
+#include "./FileSystem.h"
+
+
+class Table : public FileSystem {
+ public:
+  Table(std::string new_diskname, std::string new_flat_file, std::string new_index_file);
+  unsigned int buildTable(std::string input_file);
+  inline unsigned int search(std::string value) { return indexSearch(value); }
+ private:
+  unsigned int indexSearch(std::string value);
+  std::string flat_file;
+  std::string index_file;
+
+  DISALLOW_COPY_AND_ASSIGN(Table);
+};
+
+#endif  // SRC_TABLE_H_
