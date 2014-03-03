21f1d44b88c495ec4420e385105425e29d783a51
Andrew Brinker <me@andrewbrinker.com>
2014-03-03 11:29:00 -0800

    Preprocessor ready for testing.

--- lab4/src/gram/preprocessor.cc
+++ lab4/src/gram/preprocessor.cc
@@ -4,6 +4,7 @@
 
 #include "./preprocessor.h"
 #include <string>
+#include <set>
 #include <fstream>
 #include <cstdio>
 #include "./grammarfile.h"
@@ -11,6 +12,7 @@
 #define COMMENT   "#"
 #define SPLIT     "|"
 #define SEP       "->"
+#define DELIM     "$"
 
 namespace gram {
 
@@ -46,39 +48,15 @@ grammarfile preprocessor::load() {
 }
 
 grammarfile preprocessor::expand() {
-    return file;
-}
-
-/*
-void parser::expandFile(std::string file_name) {
-  // 1) Ignore blank lines (done)
-  // 2) Ignore comments (done)
-  // 3) Remove whitespace (done)
-  // 4) Split across OR (done)
-  // 5) Populate list of terminals
-  // 6) Insert section delimiters (done)
-  std::ifstream input_file(file_name);
-  std::list<std::string> intermediary;
   std::set<std::string> terminals;
-  for (std::string line; getline(input_file, line);) {
-    if (line[0] == COMMENT[0]) continue;
-    if (line == "\n") continue;
-    line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
-    intermediary.push_back(line + "\n");
-  }
-  input_file.close();
-  for (auto it = intermediary.begin(); it != intermediary.end(); ++it) {
-    if (it->size() <= 3) {
-      _condensed = false;
-      break;
-    }
+  for (auto it = file.contents.begin(); it != file.contents.end(); ++it) {
     size_t i = 0;
     std::string lhs = it->substr(0, 1);
     std::string rhs = it->substr(3);
     while (i < rhs.length()) {
       if (rhs[i] == SPLIT[0]) {
         *it = it->substr(0, i + 3) + "\n";
-        intermediary.push_back(lhs + "->" + rhs.substr(i+1));
+        file.contents.push_back(lhs + "->" + rhs.substr(i+1));
         break;
       } else if (!isupper(rhs[i]) && rhs[i] != '\n') {
         terminals.insert(rhs.substr(i, 1));
@@ -86,21 +64,14 @@ void parser::expandFile(std::string file_name) {
       ++i;
     }
   }
-  if (_condensed) {
-    std::string delim_line = DELIM;
-    delim_line += "\n";
-    intermediary.push_front(delim_line);
-    for (auto symbol : terminals) {
-      intermediary.push_front(symbol + "\n");
-    }
-    intermediary.push_back(delim_line);
-    std::ofstream output_file(TEMP_FILE);
-    for (auto line : intermediary) {
-      output_file << line;
-    }
-    output_file.close();
+  std::string delim_line = DELIM;
+  delim_line += "\n";
+  file.contents.push_front(delim_line);
+  for (auto symbol : terminals) {
+    file.contents.push_front(symbol + "\n");
   }
+  file.contents.push_back(delim_line);
+  return file;
 }
-*/
 
 }  // namespace gram
