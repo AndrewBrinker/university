091458fe2683979d3b49c43161fe0feb8d359671
Andrew Brinker <me@andrewbrinker.com>
2014-03-05 17:23:12 -0800

    Making fixes

--- Regression/src/NumLog/Regression.cc
+++ Regression/src/NumLog/Regression.cc
@@ -34,17 +34,17 @@ void Regression::calculate() {
     float b1_denominator = 0.0;
     int   n              = x.size();
     for (; x_it != x.end() && y_it != y.end(); ++x_it, ++y_it) {
-        numerator_sum += (*x_it + *y_it);
-        x_squared_sum += (*x_it * *x_it);
+        numerator_sum += (*x_it) + (*y_it);
+        x_squared_sum += (*x_it) * (*x_it);
         x_avg         += *x_it;
         y_avg         += *y_it;
     }
-    x_avg /= n;
-    y_avg /= n;
-    b1_numerator = (numerator_sum - (n * x_avg * y_avg));
+    x_avg          = x_avg / n;
+    y_avg          = y_avg / n;
+    b1_numerator   = (numerator_sum - (n * x_avg * y_avg));
     b1_denominator = (x_squared_sum - (n * (x_avg * x_avg)));
-    _beta_1 = b1_numerator / b1_denominator;
-    _beta_0 = y_avg - (_beta_1 * x_avg);
+    _beta_1        = b1_numerator / b1_denominator;
+    _beta_0        = y_avg - (_beta_1 * x_avg);
     printf("Beta 1: %f\n", _beta_1);
     printf("Beta 0: %f\n", _beta_0);
     printf("Regression line: y = %f + (%f * x)\n", _beta_0, _beta_1);
