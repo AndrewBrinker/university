bd50c0890fde8ecf491ca127c98c463e12ffdbd5
Andrew Brinker <me@andrewbrinker.com>
2014-04-02 12:31:14 -0700

    Trying a thing.

--- src/vm/VirtualMachine.cpp
+++ src/vm/VirtualMachine.cpp
@@ -3,3 +3,16 @@
  */
 
 #include "./VirtualMachine.h"
+
+#define REG_FILE_SIZE 4
+#define MEM_SIZE      256
+
+VirtualMachine::VirtualMachine() {
+  r   = std::vector<int>(REG_FILE_SIZE);
+  mem = std::vector<int>(MEM_SIZE);
+}
+
+VirtualMachine::VirtualMachine(unsigned int reg_file_size,
+                               unsigned int mem_size) :
+                               r(reg_file_size),
+                               mem(mem_size) {}
--- src/vm/VirtualMachine.h
+++ src/vm/VirtualMachine.h
@@ -13,17 +13,11 @@
 
 class VirtualMachine {
  public:
-  VirtualMachine() : VirtualMachine(REG_FILE_SIZE, MEM_SIZE) {}
-
-  VirtualMachine(unsigned int reg_file_size, unsigned int mem_size) :
-    r(reg_file_size),
-    mem(mem_size) {}
+  VirtualMachine();
+  VirtualMachine(unsigned int, unsigned int);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(VirtualMachine);
-
-  static const int REG_FILE_SIZE = 4;
-  static const int MEM_SIZE = 256;
 
   std::vector<int> r;
   std::vector<int> mem;
