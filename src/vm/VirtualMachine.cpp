641e9e6fd3612eeb91968e82e89162022777fdf2
Andrew Brinker <me@andrewbrinker.com>
2014-04-02 12:44:46 -0700

    Updated some stuff.

--- src/vm/VirtualMachine.cpp
+++ src/vm/VirtualMachine.cpp
@@ -7,9 +7,23 @@
 #define REG_FILE_SIZE 4
 #define MEM_SIZE      256
 
+/**
+ * Default constructor
+ *
+ * Initializes internal variables to their default values.
+ */
 VirtualMachine::VirtualMachine()
   : VirtualMachine(REG_FILE_SIZE, MEM_SIZE) {}
 
+
+/**
+ * Alternate constructor
+ *
+ * Initializes internal variables to user-defined values.
+ *
+ * @param reg_file_size -> The size of the register
+ * @param mem_size      -> The size of memory
+ */
 VirtualMachine::VirtualMachine(unsigned int reg_file_size,
                                unsigned int mem_size) :
                                r(reg_file_size),
