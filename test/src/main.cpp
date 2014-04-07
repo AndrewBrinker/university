afacde878517f59fec7d50d0d0a181ea3338600e
Andrew Brinker <me@andrewbrinker.com>
2014-04-07 11:33:03 -0700

    Updated main test file

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -2,16 +2,18 @@
  * Copyright 2014 Andrew Brinker, Anthony Sterrett
  */
 
+// #include "../src/vm/VirtualMachine.h"
 #include <cstdlib>
 #include <cstdio>
-#include "../src/vm/VirtualMachine.h"
 
 int main(int argc, char **argv) {
   if (argc != 2) {
     printf("Usage: %s [obj file]\n", argv[0]);
     exit(EXIT_FAILURE);
   }
+  /*
   VirtualMachine vm;
   vm.run(argv[1]);
+  */
   return 0;
 }
