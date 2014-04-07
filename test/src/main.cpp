a1d3bbd27af59b53ce1999ce67fdb118efd9508f
Andrew Brinker <me@andrewbrinker.com>
2014-04-07 12:04:42 -0700

    Updated test/main.cpp

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -3,15 +3,18 @@
  */
 
 #include "../../src/vm/VirtualMachine.h"
+#include "../lib/catch.h"
 #include <cstdlib>
 #include <cstdio>
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    printf("Usage: %s [obj file]\n", argv[0]);
+    printf("Usage: %s <object file name>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
+  /*
   VirtualMachine vm;
   vm.run(argv[1]);
+  */
   return 0;
 }
