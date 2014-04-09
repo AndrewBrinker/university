d2ccdf7ae69387a1113a9dbd5292d5d5064616d3
Andrew Brinker <me@andrewbrinker.com>
2014-04-08 21:26:08 -0700

    Reorganized and integrated testing.

--- src/OS.cpp
+++ src/OS.cpp
@@ -4,21 +4,26 @@
 
 #include <asm/Assembler.h>
 #include <vm/VirtualMachine.h>
+#include <test/Test.h>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <string>
 
+#define TEST_FLAG "--run-tests"
+
+void printUsage(char *name) {
+  printf("Usage: %s [--run-tests] <source file>\n", name);
+}
+
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    printf("Usage: %s <assembly file name>\n", argv[0]);
+  if (argc < 2) {
+    printUsage(argv[0]);
     exit(EXIT_FAILURE);
   }
-  /*
-  Assembler a;
-  VirtualMachine vm;
-  Assembler::ObjectSource result = a.parse(argv[1]);
-  vm.run(result);
-  */
+  if (strncmp(argv[1], TEST_FLAG, strlen(TEST_FLAG))) {
+    return runTests(argv[2]);
+  }
   return 0;
 }
