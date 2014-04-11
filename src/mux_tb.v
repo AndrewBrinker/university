bf51a8343cecdd9c196ebfe94afd370ec0981626
J. Anthony Sterrett, Jr <jasterrett@gmail.com>
2014-04-10 19:31:02 -0700

    mux and mux testbench work perfectly!

---
+++ src/mux_tb.v
@@ -0,0 +1,30 @@
+module test_mux;
+    wire [31:0] Y;
+    reg    [31:0] A, B;
+    reg                 sel;
+
+    mux mux1 ( A, B, sel, Y );
+
+    initial begin
+        A = 32'hAAAAAAAA;
+        B = 32'h55555555;
+        sel = 1'b1;
+        #10
+        A = 32'h00000000;
+        #10
+        sel = 1'b1;
+        #10
+        B = 32'hFFFFFFFF;
+        #5
+        A = 32'hA5A5A5A5;
+        #5
+        sel = 1'b0;
+        B = 32'hDDDDDDDD;
+        #5
+        sel = 1'bx;
+    end
+
+    always @( A or B or sel )
+        #1 $display( "At t = %0d, sel = %b\tA = %h\tB = %h\tY = %h",
+                     $time, sel, A, B, Y );
+endmodule
