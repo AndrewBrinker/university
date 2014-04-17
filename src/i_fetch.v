70de68c69fc929a0d705af26e18cf3618168397a
Andrew Brinker <me@andrewbrinker.com>
2014-04-17 16:31:58 -0700

    Fixed some formatting

--- src/i_fetch.v
+++ src/i_fetch.v
@@ -15,31 +15,29 @@ module i_fetch (
     wire [31:0] data_out;
     wire [31:0] npc, npc_mux;
 
-   //instantiations
-   mux mux1 (.out(npc_mux),
-             .a(EX_MEM_NPC),
-             .b(npc),
-             .sel(EX_MEM_PC_Source));
+    //instantiations
+    mux mux1 (.out(npc_mux),
+              .a(EX_MEM_NPC),
+              .b(npc),
+              .sel(EX_MEM_PC_Source));
 
-   pc_mod pc_mod1 (.PC(PC),
-                   .npc(npc_mux));
+    pc_mod pc_mod1 (.PC(PC),
+                    .npc(npc_mux));
 
-   memory memory1 (.data(data_out),
-                   .addr(PC));
+    memory memory1 (.data(data_out),
+                    .addr(PC));
 
-   if_id  if_id1  (.instruction_out(IF_ID_instruction),
-                   .npc_out(IF_ID_npc),
-                   .instruction(data_out),
-                   .npc(npc));
+    if_id  if_id1  (.instruction_out(IF_ID_instruction),
+                    .npc_out(IF_ID_npc),
+                    .instruction(data_out),
+                    .npc(npc));
 
-   incrementer incrementer1 (.out(npc),
-                             .in(PC));
+    incrementer incrementer1 (.out(npc),
+                              .in(PC));
 
-   initial begin
-      $display("Time\t PC\t npc\t data_out of MEM\t IF_ID_instruction\t IF_ID_npc");
-      $monitor("%0d\t %0d\t %0d\t %h\t %h\t %0d", $time, PC, npc, data_out, IF_ID_instruction, IF_ID_npc);
-      #20 $finish;
-   end
-
+    initial begin
+        $display("Time\t PC\t npc\t data_out of MEM\t IF_ID_instruction\t IF_ID_npc");
+        $monitor("%0d\t %0d\t %0d\t %h\t %h\t %0d", $time, PC, npc, data_out, IF_ID_instruction, IF_ID_npc);
+        #20 $finish;
+    end
 endmodule
-
--- src/pc_mod.v
+++ src/pc_mod.v
@@ -1,13 +1,13 @@
 module pc_mod (
-   output reg  [31:0] PC,
-   input  wire [31:0] npc
-   );
+        output reg  [31:0] PC,
+        input  wire [31:0] npc
+    );
 
-   initial begin
-      PC <= 0;
-   end
-   always @ ( npc) begin
-      #1 PC <= npc;
-   end
+    initial begin
+        PC <= 0;
+    end
 
+    always @ (npc) begin
+        #1 PC <= npc;
+    end
 endmodule
