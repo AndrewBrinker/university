b3f93b8bae6495dba9c96f41f2007d66d9a29343
Andrew Brinker <me@andrewbrinker.com>
2014-04-17 16:51:21 -0700

    Finished lab1

--- src/i_fetch.v
+++ src/i_fetch.v
@@ -4,16 +4,16 @@
 `include "src/if_id.v"
 `include "src/pc_mod.v"
 
-module i_fetch (
-        output wire  [31:0] IF_ID_instruction,
-        output wire  [31:0] IF_ID_npc,
-        input  wire         EX_MEM_PC_source,
-        input  wire  [31:0] EX_MEM_NPC
-    );
+module i_fetch ();
+    wire [31:0] IF_ID_instruction;
+    wire [31:0] IF_ID_npc;
+    reg         EX_MEM_PC_Source;
+    reg  [31:0] EX_MEM_NPC;
 
-    wire [31:0] PC;
-    wire [31:0] data_out;
-    wire [31:0] npc, npc_mux;
+    wire [31:0] PC,
+                data_out,
+                npc,
+                npc_mux;
 
     //instantiations
     mux mux1 (.out(npc_mux),
@@ -27,15 +27,17 @@ module i_fetch (
     memory memory1 (.data(data_out),
                     .addr(PC));
 
-    if_id  if_id1  (.instruction_out(IF_ID_instruction),
-                    .npc_out(IF_ID_npc),
-                    .instruction(data_out),
-                    .npc(npc));
+    if_id  if_id1 (.instruction_out(IF_ID_instruction),
+                   .npc_out(IF_ID_npc),
+                   .instruction(data_out),
+                   .npc(npc));
 
     incrementer incrementer1 (.out(npc),
                               .in(PC));
 
     initial begin
+        EX_MEM_PC_Source <= 1'b0;
+        EX_MEM_NPC <= 32'b0;
         $display("Time\t PC\t npc\t data of MEM\t IF_ID_instr\t IF_ID_npc");
         $monitor("%0d\t %0d\t %0d\t %h\t %h\t %0d", $time, PC, npc, data_out, IF_ID_instruction, IF_ID_npc);
         #20 $finish;
