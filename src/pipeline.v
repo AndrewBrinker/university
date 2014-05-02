6c49f1de5a688fc15c0fa8ccc438de83b0975998
Andrew Brinker <me@andrewbrinker.com>
2014-05-01 19:52:34 -0700

    Banging my head against a wall forever

--- src/pipeline.v
+++ src/pipeline.v
@@ -6,11 +6,13 @@ module pipeline ();
     wire [31:0] IF_ID_npcout;
     reg         EX_MEM_PCSrc;
     reg  [31:0] EX_MEM_NPC;
+    reg  [4:0]  regwrite;
 
     initial begin
         #0
         EX_MEM_PCSrc <= 1'b0;
         EX_MEM_NPC   <= 32'b0;
+        regwrite     <= 5'b0;
     end
 
     i_fetch i_fetch1 (.EX_MEM_PCSrc(EX_MEM_PCSrc),
@@ -20,7 +22,7 @@ module pipeline ();
 
     i_decode i_decode1 (.IF_ID_instrout(IF_ID_instrout),
                         .IF_ID_npcout(IF_ID_npcout),
-                        .MEM_WB_rd(5'b0),
+                        .MEM_WB_rd(regwrite),
                         .MEM_WB_regwrite(EX_MEM_PCSrc),
                         .WB_mux5_writedata(EX_MEM_NPC));
 
