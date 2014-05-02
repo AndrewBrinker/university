0f0b7b6bff94390d87684e9bf540334f1e47266a
Andrew Brinker <me@andrewbrinker.com>
2014-05-01 19:37:57 -0700

    Made an assortment of small changes

--- src/fetch/memory.v
+++ src/fetch/memory.v
@@ -1,18 +1,24 @@
-`define MEMORY_FILE_NAME "etc/memory.list"
-
 module memory (
         output reg  [31:0] data,
         input  wire [31:0] addr
     );
 
-    reg [31:0] mem[0:127];
+    reg [31:0] MEM[0:127];
 
     initial begin
         #0
-        $readmemh(`MEMORY_FILE_NAME, mem);
-    end
+        MEM[0] <= 'h002300AA;
+        MEM[1] <= 'h10654321;
+        MEM[2] <= 'h00100022;
+        MEM[3] <= 'h8C123456;
+        MEM[4] <= 'h8F123456;
+        MEM[5] <= 'hAD654321;
+        MEM[6] <= 'h13012345;
+        MEM[7] <= 'hAC654321;
+        MEM[8] <= 'h12012345;
+   end
 
-    always@* begin
-        data <= mem[addr];
+    always@(addr) begin
+        data <= MEM[addr];
     end
 endmodule
