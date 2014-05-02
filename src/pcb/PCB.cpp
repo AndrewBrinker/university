fa5272510950594db7e7f58dafe5a643b1ecad7f
Andrew Brinker <me@andrewbrinker.com>
2014-05-02 10:43:22 -0700

    Cleaned up PCB initializer list

--- src/pcb/PCB.cpp
+++ src/pcb/PCB.cpp
@@ -8,23 +8,26 @@
 #include <string>
 
 PCB::PCB(std::string source_file)
-  : pc(0), sr(0), sp(MEM_SIZE - 1), base(0), limit(0),
+  : pc(0),
+    sr(0),
+    sp(MEM_SIZE - 1),
+    base(0),
+    limit(0),
     r(REG_FILE_SIZE, 0),
-
     process_name(stripPath(stripExtension(source_file))),
-
     s_file(stripExtension(source_file) + SOURCE_EXT),
     o_file(stripExtension(source_file) + OBJECT_EXT,
            std::ios::in | std::ios::out | std::ios::trunc),
     in_file(stripExtension(source_file) + INPUT_EXT),
     out_file(stripExtension(source_file) + OUTPUT_EXT,
-           std::ios::out | std::ios::trunc),
+             std::ios::out | std::ios::trunc),
     st_file(stripExtension(source_file) + STACK_EXT,
-           std::ios::in | std::ios::out | std::ios::trunc),
-
-    vm_time(0), waiting_time(0), turnaround_time(0), io_time(0),
+            std::ios::in | std::ios::out | std::ios::trunc),
+    vm_time(0),
+    waiting_time(0),
+    turnaround_time(0),
+    io_time(0),
     largest_stack_size(0),
-
     interrupt_time(0)
 #ifdef DEBUG
     , log_file(stripExtension(source_file) + LOG_EXT,
