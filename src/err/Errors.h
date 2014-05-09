e64d36f6547bf19d2987a910373825b5a5ca4df5
Andrew Brinker <me@andrewbrinker.com>
2014-05-09 11:42:04 -0700

    Remove unused errors

--- src/err/Errors.h
+++ src/err/Errors.h
@@ -3,16 +3,8 @@
  */
 
 #include "./CantFitInMemory.h"
-#include "./CantMakeFile.h"
-#include "./CantOpenFile.h"
-#include "./ExceededMaxStackDepth.h"
 #include "./FailedIntegerConversion.h"
-#include "./FileDoesNotExist.h"
 #include "./GenericError.h"
-#include "./InvalidAddress.h"
-#include "./InvalidFileName.h"
 #include "./InvalidOperation.h"
 #include "./InvalidRegisterID.h"
-#include "./StackUnderflow.h"
 #include "./UnknownError.h"
-#include "./UnrecognizedFlag.h"
