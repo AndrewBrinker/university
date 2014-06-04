5cc2703cd628db701e2e777639985ffa1df6970b
J. Anthony Sterrett, Jr <jasterrett@gmail.com>
2014-06-04 06:29:50 -0700

    I told you it wasn't much.

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,24 @@
  * Copyright 2014 Andrew Brinker, Anthony Sterrett
  */
 
+/*
 #include <os/OS.h>
 
-int main() {
+int main(int argc, char* argv[]) {
   OS os;
   os.run();
+  return 0;
+}
+*/
+
+#include <tlb/TLB.h>
+
+#include <cstdio>
+
+int main() {
+  TLB_Entry te;
+
+  printf( "%d\n", te.frame );
+
   return 0;
 }
--- src/tlb/TLB.h
+++ src/tlb/TLB.h
@@ -3,33 +3,21 @@
  */
 
 #include <cstdint>
-#include <map>
+#include <vector>
 
 #ifndef TLB_H
 #define TLB_H
 
-#ifndef DISALLOW_COPY_AND_ASSIGN
-#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(const TypeName&) = delete;      \
-  void operator=(const TypeName&) = delete;
-#endif
+struct TLB_Entry {
+  TLB_Entry();
+  TLB_Entry( const uint8_t& );
 
-class TLB {
- public:
-  union tlb_entry_t {
-    uint8_t i;
-    struct {
-      uint8_t frame  : 5;
-      uint8_t modify : 1;
-      uint8_t valid  : 1;
-    } fmt0;
-  }
-
-  TLB() : data(std::map<tlb_entry_t, uint8_t>());
-  std::map<tlb_entry_t, uint8_t> data;
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(Assembler)
+  uint8_t frame    : 5;
+  bool    modified : 1;
+  bool    valid    : 1;
+  bool    empty    : 1;
 };
+
+typedef std::vector< TLB_Entry > TLB;
 
 #endif  // TLB_H
