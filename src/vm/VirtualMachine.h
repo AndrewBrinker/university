b7c93d7e157fa4e90f2de7fb8d18c4b224e9cf3c
Andrew Brinker <me@andrewbrinker.com>
2014-06-08 19:13:57 -0400

    Added page fault to return_t in VM

--- src/vm/VirtualMachine.h
+++ src/vm/VirtualMachine.h
@@ -27,7 +27,8 @@ enum return_t {
   STACK_UNDERFLOW        = 04,
   INVALID_OPCODE         = 05,
   READ_OPERATION         = 06,
-  WRITE_OPERATION        = 07
+  WRITE_OPERATION        = 07,
+  PAGE_FAULT             = 10  // Sorry to mess up the nice octal numbering
 };
 
 class VirtualMachine {
