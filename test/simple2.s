5a477f8e8c358ff334a19d774e63822677d09caf
Andrew Brinker <me@andrewbrinker.com>
2014-06-08 21:19:07 -0400

    Added the tests back

--- test/simple2.s
+++ test/simple2.s
@@ -1,11 +1,11 @@
-loadi 1 100 ! mem[1] = 100
-subi 1 1    ! mem[1] = 99
-subi 1 1    ! mem[1] = 98
-subi 1 1    ! mem[1] = 97
-subi 1 1    ! mem[1] = 96
-subi 1 1    ! mem[1] = 95
-subi 1 1    ! mem[1] = 94
-subi 1 1    ! mem[1] = 93
-subi 1 1    ! mem[1] = 92
-write 1     ! write to file
+loadi 1 100
+subi 1 1
+subi 1 1
+subi 1 1
+subi 1 1
+subi 1 1
+subi 1 1
+subi 1 1
+subi 1 1
+write 1
 halt
