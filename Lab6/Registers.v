c60a47bd0014e6bd20a7a168a2a1f7ddba4e3b7c
Eli Gonzalez <egonzalezjr555@gmail.com>
2015-03-24 18:17:48 -0700

    Newest

--- Lab6/Registers.v
+++ Lab6/Registers.v
@@ -35,32 +35,33 @@ module Registers (
         // Handle issues
         if (issue) begin
             if (redirection[A_address] == not_redirected) begin
-                A_out = reg_file[A_address];
-                A_invalid = false;
+                A_out <= reg_file[A_address];
+                A_invalid <= false;
             end
             else begin
-                A_out = redirection[A_address];
-                A_invalid = true;
+                A_out <= redirection[A_address];
+                A_invalid <= true;
             end
             if (redirection[B_address] == not_redirected) begin
-                B_out = reg_file[B_address];
-                B_invalid = false;
+                B_out <= reg_file[B_address];
+                B_invalid <= false;
             end
             else begin
-                B_out = redirection[B_address];
-                B_invalid = true;
+                B_out <= redirection[B_address];
+                B_invalid <= true;
             end
-            redirection[dest] = RS_calculating_value;
+            redirection[dest] <= RS_calculating_value;
         end
     end
 
-    always @ (posedge clock) begin
+    always @ (negedge clock) begin
         // Handle updates
         if (write) begin
+				reg_file[dest] <= In_data;
             for (i = 0; i < num_of_regs; i = i + 1) begin
                 if (redirection[i] == In_source) begin
-                    redirection[i] = not_redirected;
-                    reg_file[i] = In_data;
+                    redirection[i] <= not_redirected;
+                    reg_file[i] <= In_data;
                 end
             end
         end
